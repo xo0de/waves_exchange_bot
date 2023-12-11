d58d8bf7ff6aaf757b969d8556ff793c1b0f4eea
xo0de <101414602+xo0de@users.noreply.github.com>
2023-12-11 20:17:26 +0100

    Final commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, types, executor
 from dotenv import load_dotenv
 from aiogram.types.web_app_info import WebAppInfo
-from APIs import get_actual_price, get_payment_url_BTC, get_payment_url_USDT
+from APIs import get_actual_price, get_payment_url_btc, get_payment_url_usdt
 import logging
 import os
 
@@ -11,14 +11,12 @@ bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
 global_amount = None
 
-
-
 # Кнопки
 button = types.ReplyKeyboardMarkup(resize_keyboard=True)
 button.add('Получить')
 
 buttons_to_exchange = types.InlineKeyboardMarkup(row_width=1)
-buttons_to_exchange.add(types.InlineKeyboardButton(text='Обменять криптовалюту', callback_data='data'),
+buttons_to_exchange.add(types.InlineKeyboardButton(text='Обменять криптовалюту', callback_data='change'),
                         types.InlineKeyboardButton(text='Перейти на сайт биржи', url='https://wx.network/'))
 
 
@@ -29,15 +27,15 @@ async def cmd_start(message: types.Message):
     markup.add(types.KeyboardButton('Получить'))
     await message.answer_sticker('CAACAgIAAxkBAAIDG2VyLufSqhoIbuCe5zIO2qm5oS0XAAL7BQAClvoSBZdb7eV44WgWMwQ')
     await message.answer(
-        f"""Привет {message.from_user.first_name}, это бот, который предоставляет информацию о курсе BTC/USDT и позволяет совершить обмен.
-Чтобы получить список команда введите команду /help""", reply_markup=button)
+        f"Привет, {message.from_user.first_name}, это бот, который предоставляет информацию о курсе BTC/USDT и позволяет совершить обмен.")
+    await message.answer(f"Чтобы получить список команд введите команду /help", reply_markup=button)
 
 
-# Обработчик команды htlp
+# Обработчик команды help
 @dp.message_handler(commands=['help'])
 async def bot_help(message: types.Message):
     await message.answer(f"""
-    Доступный список комманд:
+    Доступный список команд:
 /help - вывод списка команд данного бота
 /get_price - получение курса BTC/USDT
 /exchange - произвести обмен
@@ -54,8 +52,7 @@ async def get_price(message: types.Message):
 # Обработчик команды /exchange
 @dp.message_handler(commands=['exchange'])
 async def get_price(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id,
-                           text="Введите сумму которую Вы хотите обменять:")
+    await bot.send_message(chat_id=message.from_user.id, text="Введите сумму, которую Вы хотите обменять:")
 
 
 # Обработчик кнопки "Получить"
@@ -63,6 +60,7 @@ async def get_price(message: types.Message):
 async def keyboard(message: types.Message):
     await message.answer(f"""Актуальный курс <b>BTC</b> на бирже WX Network:
 <b>{get_actual_price()} USDT</b>""", parse_mode='HTML', reply_markup=buttons_to_exchange)
+
 
 # Обработчик суммы к обмену
 @dp.message_handler(lambda message: message.text.isdigit())
@@ -74,9 +72,14 @@ async def handle_number(message: types.Message):
         await message.reply(
             f"Введите положительное число:")
     buttons_ready_to_exchange = types.InlineKeyboardMarkup(row_width=1)
-    buttons_ready_to_exchange.add(types.InlineKeyboardButton(text=f'Обменять {global_amount} BTC на USDT', web_app=WebAppInfo(url=f'{get_payment_url_BTC(global_amount)}')),
-                                        types.InlineKeyboardButton(text=f'Обменять {global_amount} USDT на BTC', web_app=WebAppInfo(url=f'{get_payment_url_USDT(global_amount)}')))
+    buttons_ready_to_exchange.add(types.InlineKeyboardButton(text=f'Обменять {global_amount} BTC на USDT',
+                                                             web_app=WebAppInfo(
+                                                                 url=f'{get_payment_url_btc(global_amount)}')),
+                                  types.InlineKeyboardButton(text=f'Обменять {global_amount} USDT на BTC',
+                                                             web_app=WebAppInfo(
+                                                                 url=f'{get_payment_url_usdt(global_amount)}')))
     await message.reply(f"Сумма к обмену: {global_amount}", reply_markup=buttons_ready_to_exchange)
+
 
 # Обработчик любого текста
 @dp.message_handler(lambda message: message.text.isalpha(), content_types=['text'])
@@ -84,12 +87,14 @@ async def handle_non_number(message: types.Message):
     await message.reply("""В случае, если Вы хотите произвести обмен, пожалуйста, введите корректное число. 
 Иначе воспользуйтесь одной из команд, список команд вы можете увидеть, введя команду /help""")
 
+
 # Обработчик callback данных от кнопки "Обменять"
 @dp.callback_query_handler()
 async def double_exchange(callback_query: types.CallbackQuery):
-    if callback_query.data == 'data':
+    if callback_query.data == 'change':
         await bot.send_message(chat_id=callback_query.from_user.id,
-                               text="Введите сумму которую Вы хотите обменять:")
+                               text="Введите сумму, которую Вы хотите обменять:")
+
 
 # Запуск программы
 if __name__ == '__main__':
